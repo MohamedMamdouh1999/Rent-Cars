5f5f8e7ff7509f595b2dd5ffb4fccd1fe22f96b4
Mohamed Mamdouh <110316477+MohamedMamdouh1999@users.noreply.github.com>
2024-05-18 23:09:19 +0300

    upload car details page and make refactor

--- src/app/modules/car-details/components/car/car.component.ts
+++ src/app/modules/car-details/components/car/car.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { ICar } from 'src/app/modules/shared/interfaces/icar';
+import { CarsService } from 'src/app/modules/shared/services/cars.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss']
 })
-export class CarComponent {
+export class CarComponent implements OnInit, OnDestroy {
+  car: ICar = {} as ICar;
+  @Input({required: true}) id: number = 0;
+  private destroySubject: Subject<void> = new Subject();
 
+  constructor(private carsService: CarsService) {};
+
+  ngOnInit(): void {
+    if(this.id){
+      this.getSingleCar(this.id);
+    }
+    window.scrollTo(0, 0);
+  }
+
+  getSingleCar(id: number): void {
+    this.carsService.getSingleCar(id).pipe(takeUntil(this.destroySubject)).subscribe({
+      next: response => this.car = response
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
 }
