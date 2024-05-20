a98aed15f5d7892e5855fd07d7617f52457d6ca3
Mohamed Mamdouh <110316477+MohamedMamdouh1999@users.noreply.github.com>
2024-05-20 23:29:31 +0300

    upload not found page and change extension of images

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ErrorComponent } from './components/error/error.component';
 import { CarsGroupComponent } from './components/cars-group/cars-group.component';
 import { SingleCarComponent } from './components/single-car/single-car.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    ErrorComponent,
     NotFoundComponent,
-    CarsGroupComponent,
-    SingleCarComponent
+    SingleCarComponent,
+    CarsGroupComponent
   ],
   imports: [
     CommonModule,
